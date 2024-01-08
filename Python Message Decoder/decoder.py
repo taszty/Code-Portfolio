979d86e8d70b66561a4662929efa0fcf9109e849
nelson-dylan <36965721+taszty@users.noreply.github.com>
2024-01-08 10:00:43 -0700

    Add python decoder

---
+++ Python Message Decoder/decoder.py
@@ -0,0 +1,19 @@
+# define function 
+def decode(message_file):
+    # read input content
+    with open(message_file, 'coding_qual_input') as file:
+        lines = file.readlines()
+        # extract the numbers used for the code into their own list
+    numbers = [int(line.split()[0]) for line in lines]
+    # extract words according to their corresponding number from the previous list
+    decoded_words = [line.split()[1] for i, line in enumerate(lines) if i + 1 in numbers]
+    # join the words into a decoded string
+    decoded_message = ' '.join(decoded_words)
+    # output decoded string
+    return decoded_message
+
+# example using referenced file
+message_file_path = 'coding_qual_input.txt'
+decoded_message = decode(message_file_path)
+# output decoded message
+print(decoded_message)
